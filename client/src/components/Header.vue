e29adf7462bd597c5ad1ebfaa017298ef7f1d1c4
emartinez <emilio.martinez@atacas.com>
2018-04-28 23:37:25 +0200

    refactoring some routes

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -43,7 +43,7 @@ export default {
       this.$store.dispatch('setToken', null)
       this.$store.dispatch('setUser', null)
       this.$router.push({
-        name: 'root'
+        name: 'songs'
       })
     }
   }
